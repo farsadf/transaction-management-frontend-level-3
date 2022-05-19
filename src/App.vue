e1f633cc39dbed93eb8a8650eca08c81a08f4fdd
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-19 14:49:53 +0430

    vuex handlers added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="container w-screen flex lg:flex-row flex-col">
     <TransactionForm @newTransaction="addNewTransaction" />
-    <TransactionHistory
-      :key="transactions.length"
-      v-if="transactions !== []"
-      :items="transactions"
-    />
+    <TransactionHistory v-if="transactions !== []" :items="transactions" />
     <div class="err" v-else>
       sorry transaction history is not available right now
     </div>
@@ -15,6 +11,7 @@
 <script>
 import TransactionForm from "./components/TransactionForm.vue";
 import TransactionHistory from "./components/history/TransactionHistory.vue";
+import store from "./store";
 export default {
   components: { TransactionForm, TransactionHistory },
   data() {
@@ -24,19 +21,9 @@ export default {
     };
   },
   mounted() {
-    fetch("https://infra.devskills.app/api/accounting/transactions")
-      .then((res) => res.json())
-      .then((data) => {
-        this.transactions = data.sort((a, b) => {
-          a.created_At - b.created_At;
-        });
-        fetch(
-          "https://infra.devskills.app/api/accounting/accounts/" +
-            this.transactions[0].account_id
-        )
-          .then((res) => res.json())
-          .then((data) => (this.transactions[0].balance = data.balance));
-      })
+    store
+      .dispatch("loadTransaction")
+      .then((data) => (this.transactions = data))
       .catch((err) => {
         console.log(err);
         this.err = true;
@@ -44,20 +31,7 @@ export default {
   },
   methods: {
     addNewTransaction(data) {
-      fetch(
-        "https://infra.devskills.app/api/accounting/transactions/" +
-          data.transaction_id
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          this.transactions.unshift(data);
-          fetch(
-            "https://infra.devskills.app/api/accounting/accounts/" +
-              data.account_id
-          )
-            .then((res) => res.json())
-            .then((data) => (this.transactions[0].balance = data.balance));
-        });
+      store.dispatch("addNewTransaction", data);
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -2,12 +2,54 @@ import { createStore } from "vuex";
 const store = createStore({
   state() {
     return {
-      count: 0,
+      transactions: [],
     };
   },
   mutations: {
-    increment(state) {
-      state.count++;
+    setTransactions(state, transactions) {
+      state.transactions = transactions;
+    },
+    setbalance(state, balance) {
+      state.transactions[0].balance = balance;
+    },
+    setNewTransaction(state, object) {
+      state.transactions.unshift(object);
+    },
+  },
+  actions: {
+    loadTransaction(context) {
+      return fetch("https://infra.devskills.app/api/accounting/transactions")
+        .then((res) => res.json())
+        .then((data) => {
+          let transactions = data.sort((a, b) => {
+            a.created_At - b.created_At;
+          });
+          context.commit("setTransactions", transactions);
+
+          fetch(
+            "https://infra.devskills.app/api/accounting/accounts/" +
+              context.state.transactions[0].account_id
+          )
+            .then((res) => res.json())
+            .then((data) => context.commit("setbalance", data.balance));
+          return context.state.transactions;
+        });
+    },
+    addNewTransaction({ commit }, data) {
+      fetch(
+        "https://infra.devskills.app/api/accounting/transactions/" +
+          data.transaction_id
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          commit("setNewTransaction", data);
+          fetch(
+            "https://infra.devskills.app/api/accounting/accounts/" +
+              data.account_id
+          )
+            .then((res) => res.json())
+            .then((data) => commit("setbalance", data.balance));
+        });
     },
   },
 });
