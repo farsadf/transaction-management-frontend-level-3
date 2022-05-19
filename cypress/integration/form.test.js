fb3f475aff88abcd393efe566539854d797f4d66
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-19 14:11:14 +0430

    unit test added

---
+++ cypress/integration/form.test.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("testing form", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("form does not work if inputs are not validated", () => {
+    cy.get("[data-type=transaction-submit]").click();
+    cy.contains("you're account number or amount is unvalid").should("exist");
+    cy.get("[data-type=amount]").type("abc");
+    cy.contains("you're account number or amount is unvalid").should("exist");
+  });
+  it("new transaction is added if the data be valid", () => {
+    cy.get("[data-type=account-id]").type(
+      "0afd02d3-6c59-46e7-b7bc-893c5e0b7ac2"
+    );
+    cy.get("[data-type=amount]").type(522);
+    cy.get(
+      "[data-type=transaction][data-account-id=0afd02d3-6c59-46e7-b7bc-893c5e0b7ac2][data-amount=522]"
+    );
+  });
+});
