c6c0e1f23f3ff36d6973df31038b84ebcf0173a7
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-19 09:33:59 +0430

    items handled

--- src/components/history/TransactionItem.vue
+++ src/components/history/TransactionItem.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="transaction-item"></div>
+  <div
+    class="transaction-item border-2 w-full border-gray-400 h-10 text-center"
+  >
+    Transferred {{ amount }}$ to account {{ id }}
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+    accountId: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    createdAt: {
+      type: String,
+    },
+  },
+};
 </script>
 
 <style></style>
