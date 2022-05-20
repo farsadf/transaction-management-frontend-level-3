38f3d7a099a2c955c5e7defaef161b7dfa3b2553
armanamraei <armanamraei.pv@gmail.com>
2022-05-20 21:35:55 +0430

    fix: nuxt-components directory setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,8 +20,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [{ path: '~/components', pathPrefix: false }],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
