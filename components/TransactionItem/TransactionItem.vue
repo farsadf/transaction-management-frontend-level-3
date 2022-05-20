befbbcc3155a93638c84caabefd56f46af1a37e3
armanamraei <armanamraei.pv@gmail.com>
2022-05-20 23:07:37 +0430

    feat: handle submitting new transaction and rendering the response

--- components/TransactionItem/TransactionItem.vue
+++ components/TransactionItem/TransactionItem.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="t-transaction-item w-100 shadow-sm" v-html="customText"></div>
+  <div class="t-transaction-item w-100 shadow-sm">
+    <span
+      class="t-transaction-item__badge"
+      v-if="transaction.isNew && showNewBadge"
+      >NEW</span
+    >
+    <span v-html="customText"></span>
+  </div>
 </template>
 
 <script>
@@ -11,10 +18,22 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      showNewBadge: true,
+    }
+  },
   computed: {
     customText() {
       return `Transfer <b>$${this.transaction.amount}</b> from account <b>${this.transaction.account_id}</b>`
     },
+  },
+  mounted() {
+    if (this.transaction.isNew) {
+      setTimeout(() => {
+        this.showNewBadge = false
+      }, 5000)
+    }
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,13 @@
               <div class="t-form-content w-100 shadow-sm">
                 <form class="w-100" @submit="addTransaction">
                   <div class="t-input w-100 d-flex">
-                    <input data-type="account-id" />
+                    <input
+                      v-model="newTransaction.account_id"
+                      data-type="account-id"
+                    />
                   </div>
                   <div class="t-input w-100 d-flex">
-                    <input data-type="amount" />
+                    <input v-model="newTransaction.amount" data-type="amount" />
                   </div>
 
                   <input data-type="transaction-submit" type="submit" />
@@ -52,6 +55,10 @@ export default {
       loading: {
         initial: true,
       },
+      newTransaction: {
+        account_id: null,
+        amount: null,
+      },
       transactions: [],
     }
   },
@@ -67,15 +74,27 @@ export default {
           this.loading.initial = false
         })
     },
-    addTransaction(e) {
+    async addTransaction(e) {
       e.preventDefault()
+      if (this.loading.add) return
+      this.loading.add = true
 
-      this.$axios
-        .post('https://infra.devskills.app/api/accounting/transactions', {})
-        .then((res) => {})
+      const temp = {}
+      if (this.newTransaction.account_id)
+        temp.account_id = this.newTransaction.account_id
+      if (this.newTransaction.amount) temp.amount = this.newTransaction.amount
+
+      await this.$axios
+        .post('https://infra.devskills.app/api/accounting/transactions', temp)
+        .then((res) => {
+          this.newTransaction = {}
+          this.transactions.unshift({ ...res.data, isNew: true })
+        })
         .catch((err) => {
           err && err.response && this.$errorReport.build(err.response.data)
         })
+
+      this.loading.add = false
     },
   },
 }
