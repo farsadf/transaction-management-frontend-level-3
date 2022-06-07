cdd0670439caa16bf2f6880a1f4dab96a2189e73
Sam Afshari <sam@inkitt.com>
2022-06-07 11:59:21 +0430

    feat: install libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,33 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
+    "plugin:vue/essential",
+    "airbnb-base",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    indent: ['error', 2],
+    indent: ["error", 2],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        ts: "never",
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
   },
 };
