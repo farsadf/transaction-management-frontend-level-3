b77412013c55eb46b4e9cb32a4a9129f4f79dc64
mhmmdrzkh9977 <mhmmdrz1999@gmail.com>
2022-08-06 23:05:14 +0430

    create form and get list in store

---
+++ ui/components/App/TTransactionPage/Card/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    asd
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionCard'
+}
+</script>
+
+<style scoped>
+
+</style>
--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -1,11 +1,70 @@
 <template>
-  <v-container>
-    test
+  <v-container class="form-container">
+    <v-row>
+      <v-col cols="4" class="pa-3">
+        <div class="box pa-3">
+          <form>
+            <v-text-field v-model="form.accountId" data-type="account-id" label="Account ID" outlined />
+            <v-text-field v-model="form.accountId" data-type="amount" label="Amount" outlined />
+            <v-btn
+              type="submit"
+              width="80%"
+              class="text-center d-block mx-auto"
+              depressed
+              height="50px"
+              data-type="transaction-submit"
+              color="success"
+              :loading="loading"
+              @click.prevent="submit "
+            >
+              submit
+            </v-btn>
+          </form>
+        </div>
+      </v-col>
+      <v-col cols="8" class="pa-3">
+        <div class="box pa-3">
+          asd
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      form: {
+        accountId: '',
+        amount: 0
+      },
+      loading: false
+    }
+  },
+  mounted () {
+    this.fetchTransactionsList()
+  },
+  methods: {
+    ...mapActions({
+      fetchTransactionsList: 'transaction/fetchTransactionsList'
+    }),
+    submit () {
+      this.loading = true
+      this.fetchTransactionsList(this.form).finally(() => {
+        this.loading = false
+      })
+    }
+  }
 }
 </script>
+<style scoped lang="scss">
+.form-container {
+  .box {
+    border: 1px solid #343434;
+    border-radius: 8px;
+  }
+}
+</style>
